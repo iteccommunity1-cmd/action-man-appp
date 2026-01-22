d082b62be01e147dce70939a3c028b4de11f2536
[dyad] <itec.community1@gmail.com>
2026-01-22 01:36:51 +0000

    [dyad] Completed final mobile optimizations, ensuring all dialogs and key pages are fully responsive and user-friendly on smaller screens. - wrote 4 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { ProjectList } from "@/components/ProjectList";
 
 const Index = () => {
   return (
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col items-center w-full p-4 sm:p-0"> {/* Added responsive padding */}
       <div className="flex flex-col lg:flex-row items-start justify-center w-full max-w-7xl gap-8">
         <div className="w-full">
           <ProjectList />
